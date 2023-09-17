28cc43bd8e53d005ec2f17c3b58db8e966ac2d39
Santiago <david.pina@uptc.edu.co>
2023-09-17 09:58:41 -0500

    Load project on GitHUB repository

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './components/login/Login.jsx'
+
+function App() {
+  return (
+    <>
+    <Login></Login>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { environment } from "../../enviroments/enviroment.dev";
+import ReCAPTCHA from "react-google-recaptcha";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import "bootstrap/dist/css/bootstrap.css";
+import "./login.css";
+import jwt_decode from "jwt-decode";
+import FacebookLogin from "react-facebook-login";
+
+function Login() {
+  const [typeText, setTypeText] = useState("password");
+  const [eyePassword, setEyePassword] = useState("-slash");
+
+  const changeTypeText = () => {
+    if (typeText === "password") {
+      setTypeText("text");
+      setEyePassword("");
+    } else {
+      setTypeText("password");
+      setEyePassword("-slash");
+    }
+  };
+
+  const onChangeCaptcha = (value) => {
+    console.log("Captcha value:", value);
+  };
+
+  const onSuccessGoogle = (credentialResponse) => {
+    var credentialResponseDecode = jwt_decode(credentialResponse.credential);
+    console.log(credentialResponseDecode);
+  };
+
+  const onErrorGoogle = () => {
+    console.log("Login Failed");
+  };
+
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
+
+  return (
+    <div className="principalContainer">
+      <div className="contenedorLogin">
+        <div className="divFormulario">
+          <h2 className="textLogin">Iniciar Sesión</h2>
+          <div className="form">
+            <form className="login-form">
+              <input
+                type="email"
+                name="email"
+                placeholder="Correo electronico"
+                className="form-control"
+                id="email"
+              />
+              <input
+                type={typeText}
+                name="password"
+                placeholder="Contraseña"
+                className="form-control"
+                id="password"
+              />
+              <i
+                className={`fa-lg fa-sharp fa-solid fa-eye${eyePassword}`}
+                id="iconEyeSlash"
+                onClick={changeTypeText}
+              ></i>
+              <p className="msg-forgot-password">
+                <a href="#">Olvido su contraseña?</a>
+              </p>
+
+              <button className="btn-login">Entrar</button>
+              <button className="btn-login">Registrarse</button>
+              <div id="gwd-reCAPTCHA_2">
+                <ReCAPTCHA
+                  sitekey={environment.recaptcha.siteKey}
+                  onChange={onChangeCaptcha}
+                />
+              </div>
+              <hr></hr>
+              <GoogleOAuthProvider clientId={environment.google_ID_API}>
+                <GoogleLogin
+                  text="continue_with"
+                  locale="es_ES"
+                  onSuccess={onSuccessGoogle}
+                  onError={onErrorGoogle}
+                  size="large"
+                />
+              </GoogleOAuthProvider>
+
+              <FacebookLogin
+                appId={environment.facebook_ID_API}
+                autoLoad={false}
+                fields="name,email,picture"
+                callback={responseFacebook}
+                icon="fa-facebook"
+                cssClass="btn-facebook"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
